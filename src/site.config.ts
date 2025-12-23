10f1cf73952df96ef33826e1a1df2706f3e62f2a
coycatrett <ctcatrett@gmail.com>
2025-12-23 10:54:22 -0500

    testing giscus

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'https://multiterm.stelclementine.com',
+  site: 'https://coys-blog.com/',
   // The name of your site, used in the title and for SEO.
   title: 'coy\'s blog',
   // The description of your site, used for SEO and RSS feed.
@@ -11,7 +11,7 @@ const config: SiteConfig = {
   // The author of the site, used in the footer, SEO, and RSS feed.
   author: 'Coy Catrett',
   // Keywords for SEO, used in the meta tags.
-  tags: ['Astro', 'Terminal', 'Theme', 'MultiTerm', 'stelcodes'],
+  tags: ['coys blog', 'Coy Catrett', 'coycatrett', 'coys-blog', 'Multiterm'],
   // Path to the image used for generating social media previews.
   // Needs to be a square JPEG file due to limitations of the social card generator.
   // Try https://squoosh.app/ to easily convert images to JPEG.
